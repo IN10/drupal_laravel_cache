fcbb202fd303e624b07c91664f6b2bbf5a673755
Stephan de Bruin <stephandebruin21@gmail.com>
2019-05-22 15:06:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# IN10 - Drupal Laravel Cache
+
+Module which enable automatic cache invalidation in Laravel based on tag (bundle) 
+
+## Installation
+
+Get it with composer
+
+```bash
+composer require in10/drupal_laravel_cache
+```
---
+++ src/Controller/DrupalLaravelCacheController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\drupal_laravel_cache\Controller;
+
+use Drupal\Core\Site\Settings;
+use GuzzleHttp\Client;
+
+class DrupalLaravelCacheController
+{
+  /**
+   * @var string
+   */
+  protected static $laravelUrl;
+
+    /**
+     * DrupalLaravelCacheController constructor.
+     */
+  public function __construct()
+  {
+    $laravelSettings = Settings::get('laravel');
+    self::$laravelUrl = $laravelSettings['base_url'] . $laravelSettings['cache_endpoint'];
+  }
+
+  /**
+   * Call Laravel to clear all a specific bundle (content type / tag) from cache
+   * @param $entity
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public static function invalidateCache($entity)
+  {
+    // get the entity bundle
+    $bundle = $entity->bundle();
+
+    try {
+      $client = new Client();
+      $client->request('POST', self::$laravelUrl, [
+        'form_params' => [
+          'tags' => $bundle
+        ]
+      ]);
+      \Drupal::logger('drupal_laravel_cache')->notice('Caches invalidated for tag ' . $bundle);
+    } catch (\Exception $exception) {
+      \Drupal::logger('drupal_laravel_cache')->error($exception->getMessage());
+      \Drupal::logger('drupal_laravel_cache')->notice('Something went wrong while invalidating Laravel cache. Please check Laravel logs for the cause.');
+    }
+  }
+
+  /**
+   * Call Laravel to clear all content from cache
+   * @return array
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function clearAll()
+  {
+    try {
+      $client = new Client();
+      $client->request('POST', self::$laravelUrl);
+      $message = 'Caches invalidated for all content';
+      \Drupal::logger('drupal_laravel_cache')->notice($message);
+    } catch (\Exception $exception) {
+      \Drupal::logger('drupal_laravel_cache')->error($exception->getMessage());
+      $message = 'Something went wrong while clearing the cache.';
+      \Drupal::logger('drupal_laravel_cache')->notice('Something went wrong while invalidating Laravel cache. Please check Laravel logs for the cause.');
+    }
+
+    return array(
+      '#markup' => t($message),
+    );
+  }
+}
